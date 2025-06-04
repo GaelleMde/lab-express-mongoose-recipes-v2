522f98b99b885e569d27789c1d5e12fa2cc1d4c7
Gaelle <gaelle.madelaine@gmail.com>
2025-06-04 20:16:29 +0200

    completed

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,35 @@
 // Your code here ...
+const mongoose = require("mongoose");
+
+const RecipeSchema = new Recipe.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  instructions: String,
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients: {
+    type: [String],
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: {
+    type: Number, 
+    min: 0,
+  }
+  isArchived: Boolean,
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Recipe = mongoose.model("Recipe", RecipeSchema);
+
+module.exports = Recipe;
